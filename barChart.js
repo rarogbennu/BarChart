b42b9ab3290ed15b86afc727e8c5cf9101dac282
Rolf <rarogbennu@gmail.com>
2023-04-12 11:33:08 +0200

    Set height percentually and max

--- barChart.js
+++ barChart.js
@@ -2,10 +2,13 @@
 
 window.addEventListener("load", start);
 
+const data = []
+
 function start() {
     const bars = document.querySelectorAll(".bar")
 
     const initBar = bars[0];
-    const height = 50
+    const maxHeight = 32;
+    const height = data[0] / maxHeight * 100;
     initBar.style.height = ` ${height}px`;
 }
