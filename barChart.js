01b10b416100056fb1790069e996bb6349bfa34f
Rolf <rarogbennu@gmail.com>
2023-04-12 11:23:40 +0200

    Change bar height JS

--- barChart.js
+++ barChart.js
@@ -1,1 +1,11 @@
 "use strict"
+
+window.addEventListener("load", start);
+
+function start() {
+    const bars = document.querySelectorAll(".bar")
+
+    const initBar = bars[0];
+    const height = 50
+    initBar.style.height = ` ${height}px`;
+}
