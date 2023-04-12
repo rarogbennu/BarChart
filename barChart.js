8b282bac7f67c545939938e6d561de7b05357c3a
Rolf <rarogbennu@gmail.com>
2023-04-12 16:22:16 +0200

    data and for loop

--- barChart.js
+++ barChart.js
@@ -2,13 +2,19 @@
 
 window.addEventListener("load", start);
 
-const data = []
+const data = [6, 20, 0, 27, 1, 28, 3, 5, 7, 32, 27, 13, 11, 29, 22, 25, 32, 19, 18, 1, 4, 11, 32, 12, 3, 14, 20, 19, 23, 13, 28, 2, 7, 24, 18, 29, 9, 5, 1, 0]
 
 function start() {
-    const bars = document.querySelectorAll(".bar")
-
-    const initBar = bars[0];
-    const maxHeight = 32;
-    const height = data[0] / maxHeight * 100;
-    initBar.style.height = ` ${height}px`;
+    showData()
 }
+
+function showData() {
+    const bars = document.querySelectorAll(".bar")
+    const maxHeight = 32;
+    
+    for( let i=0; i<40; i++) {
+    const individualBar = bars[i];
+    const height = data[i] / maxHeight * 100;
+    individualBar.style.height = ` ${height}px`;}
+}
+
